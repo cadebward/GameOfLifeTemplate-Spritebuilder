80c07e0ad7823953f12b89cc3d544df48298c24c
Cade Ward <cadew@live.com>
2014-06-26 10:28:57 -0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.h
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -11,6 +11,10 @@
 @interface Grid : CCSprite
 
 @property (nonatomic, assign) int totalAlive;
-@property (nonatomic, assign) int generator;
+@property (nonatomic, assign) int generation;
+
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
 
 @end
